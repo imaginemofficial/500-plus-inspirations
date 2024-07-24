387219dd541a5e5b365a9f87527a1920555a09ae
Jonatas Evangelista <jonatasge@gmail.com>
2024-07-24 00:47:44 -0300

    improvements

--- app.js
+++ app.js
@@ -1,64 +1,47 @@
-function init(SeletorFrase, seletorAutor, seletorBtn) {
-    // Selecionando elementos do DOM
-    const frase = document.querySelector(SeletorFrase);
-    const autor = document.querySelector(seletorAutor);
-    const btn = document.querySelector(seletorBtn);
-    const body = document.querySelector('body');
+function init(seletorFrase, seletorAutor) {
+  // Selecionando elementos do DOM
+  const frase = document.querySelector(seletorFrase);
+  const autor = document.querySelector(seletorAutor);
 
-    // Tratativa de erro
-    if (frase && autor && btn) {
-        // Função Assincrona puxando a frase da API
-        async function activeApp() {
-            try {
-                // Frase API
+  // Mensagem inicial de acordo com o horário
+  const now = new Date().getHours();
+  if (now > 3 && now <= 12) frase.innerText = "Bom dia!";
+  if (now > 12 && now <= 18) frase.innerText = "Boa tarde!";
+  else frase.innerText = "Boa noite!";
 
-                // Faz um fetch na url
-                const dadosResponse = await (fetch('./phrases.json'));
-                // Aguarda o retorno do Fetch e transforma em JSON
-                const dadosJSON = await (await dadosResponse).json();
-                // Puxando as frases de forma aleatoria
-                const aleatorio = dadosJSON[Math.floor(Math.random() * 2)];
+  function getArrayIndexForToday(arrayLength) {
+    const dayOfYear = Math.floor(
+      (new Date() - new Date(new Date().getFullYear(), 0, 0)) / 86400000
+    );
+    return (dayOfYear - 1) % arrayLength;
+  }
 
-                // Insere os dados no DOM
-                frase.innerText = aleatorio.quote;
-                autor.innerText = aleatorio.author;
-                return gradientColor();
+  // Tratativa de erro
+  if (frase && autor) {
+    // Função Assincrona puxando a frase da API
+    async function activeApp() {
+      try {
+        // Frase API
 
-            } catch (erro) {
-                console.log(erro);
-            }
+        // Faz um fetch na url
+        const dadosResponse = await fetch("./phrases.json");
+        // Aguarda o retorno do Fetch e transforma em JSON
+        const dadosJSON = await (await dadosResponse).json();
+        // Puxando as frases de forma aleatoria
+        const aleatorio = dadosJSON[getArrayIndexForToday(dadosJSON.length)];
 
-        }
-
-        async function gradientColor() {
-            // Gradient Colors API
-
-            try {
-                // Faz um fetch na url
-                const colorsResponse = await (fetch('./colors.json'));
-                // Aguarda o retorno do Fetch e transforma em JSON
-                const colorsJSON = await (await colorsResponse).json();
-                // Puxando as cores de forma aleatoria
-
-                const aleatorioColors = colorsJSON[Math.floor(Math.random() * 85)].color;
-
-                // Adicionado cor ao Body
-                body.style.background = aleatorioColors;
-            } catch (erro) {
-                console.log(erro)
-            }
-
-
-        }
-
-        // Evento do botão
-        btn.addEventListener('click', activeApp);
-
-        // Ativando a função quando entra no site
-        activeApp();
+        // Insere os dados no DOM
+        frase.innerText = aleatorio.quote;
+        autor.innerText = aleatorio.author;
+      } catch (erro) {
+        console.log(erro);
+      }
     }
 
-
+    // Ativando a função quando entra no site
+    activeApp();
+  }
 }
+
 // Chamando a função geral para inicar o codigo
-init('.frase', '.autor', '.btn-novo');
+init(".frase", ".autor");
